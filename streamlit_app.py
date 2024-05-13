abc7f253eb333c7f728f247d48b2ce25cc293175
prasadharihar <prasadharihar9823@gmail.com>
2024-05-13 15:17:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col  # Import col function
-import requests
 
 
 
@@ -60,6 +59,8 @@ my_insert_stmt = """ INSERT INTO smoothies.public.orders(ingredients,name_on_ord
 time_to_insert =st.button('Submit Order')
 session.sql(my_insert_stmt).collect()
 st.success('Your Smoothie is ordered!', icon="✅")
+import requests
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())

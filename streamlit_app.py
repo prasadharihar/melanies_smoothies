810af68f706127b78f5c32482824528a8598d9ec
prasadharihar <prasadharihar9823@gmail.com>
2024-05-13 16:02:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ session.sql(my_insert_stmt).collect()
 st.success('Your Smoothie is ordered!', icon="✅")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+fv_df =st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
